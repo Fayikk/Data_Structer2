252f9115df3a025c1ad64d2679acf5c3e6926b43
Fayik <veznedaroglufayik2@gmail.com>
2022-07-14 22:47:06 +0300

    Updated

---
+++ Hashset_Application/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hashset_Application
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hashset
+            //Tanımlama
+            var sesliharf = new HashSet<char>()
+            {
+                'e','ı','i','o','ö','u','ü','b'
+            };
+            
+            
+            
+
+            //foreach ifadesi ile işlemimizin bir çıktısını alalım.
+            
+            sesliharf.Add('a');//Hashsete char(karakter) ekleme işlemini gerçekleştirdik
+            KoleksiyonYazdir(sesliharf);
+
+            sesliharf.Remove('b');//Hashsetten char(karakter) çıkarma işlemini  gerçekleştirdik.
+            KoleksiyonYazdir(sesliharf);
+            var deneme = new List<int>();
+
+            
+
+
+            
+            //ASCİ karakterler ile alfabe yazımı kullanıp,ekleme işlemi gerçekleştirdik.
+            var alfabe = new List<char>();
+            for (int i = 97; i < 123; i++)
+            {
+                alfabe.Add((char)i);//cast işlemi yapıp ilgil karakter için atama yapmış olduk
+            }
+            //Türkçede kullanılan sesli harflerde bulunmaktadır.
+            alfabe.ForEach(a=>Console.WriteLine(a));
+
+
+            //Kümeleme işlemlerimize bir göz atalım.
+            //Yalnızca türkçede kullanılan sesli harfler nelerdir.
+            // sesliharf.ExceptWith(alfabe);//küme işlemlerinde sesliharf fark alfabe anlamına gelmektedir.
+           // sesliharf.UnionWith(alfabe);//Kümeleme işlemlerinde iki kümenin birleşimi anlamına gelmektedir.
+            //sesliharf.IntersectWith(alfabe);//Her iki küme için kesişim ifadesi anlamına gelmektedir.
+
+            Console.WriteLine("********************");
+            KoleksiyonYazdir(sesliharf);
+        }   
+
+        static void KoleksiyonYazdir(HashSet<char> koleksiyon)//Program class'ından herhangi bir nesne üretmeden.Fonksiyon üretimi yapmak için static kullanımı yapıyoruz.
+        {
+            foreach (char item in koleksiyon)
+            {
+                Console.WriteLine(item);
+
+            }
+            Console.WriteLine("Eleman sayisi: ");
+            Console.WriteLine(koleksiyon.Count);
+        }
+    }
+}
---
+++ Sorted_Set_Group_Applications/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorted_Set_Group_Applications
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sorted set Küme işlemleri
+            var A = new SortedSet<int>() { 1,2,3,4};//Rastgele 100 sayı oluşturduk.
+            var B = new SortedSet<int>() { 1,2,5,6};
+            #region
+
+          
+
+
+            Console.WriteLine();
+            Console.WriteLine("A kümesi");
+            foreach (int s in A)
+            {
+                Console.WriteLine($"{s,5}");
+            }
+            Console.WriteLine();
+            Console.WriteLine("B kümesi");
+            foreach (int s in B)
+            {
+                Console.WriteLine($"{s,5}");
+            }
+            #endregion
+
+            A.UnionWith(B);
+            Console.WriteLine("A ve B kümlesinin birleşimi");
+            foreach (var s in A)
+            {
+                Console.WriteLine($"{s,5}");
+            }
+
+            A.IntersectWith(B);//A kümesinde olup,B kümesinde olmayan ne var.
+            Console.WriteLine("A ve B kümelerinin kesişimi");
+            foreach (var s in A)
+            {
+                Console.WriteLine($"{s,5}");
+            }
+
+            A.ExceptWith(B);
+            Console.WriteLine("Bu bir denemedir.-----");
+            foreach (var s in A)
+            {
+                Console.WriteLine($"{s,5}");
+            }
+
+
+        }
+        static List<int> RastgeleSayiUret(int n)
+        {
+            var list = new List<int>();
+            var r = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                list.Add(r.Next(0, 1000));
+                
+            }
+            return list;
+        }
+    }
+}
